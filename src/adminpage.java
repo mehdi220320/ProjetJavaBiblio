5c86ccb7b760f3f095801c4e485389ff7cc290e1
mehdi220320 <111698017+mehdi220320@users.noreply.github.com>
2024-05-13 10:16:15 +0100

    Signup.form

--- src/adminpage.java
+++ src/adminpage.java
@@ -1,12 +1,47 @@
+import javax.sql.RowSetMetaData;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class adminpage extends JFrame {
+
+    private JTable table1;
+    private JTextField UserID;
+    private JTextField DocID;
+    private JTextField NbDoc;
+    private JButton addBookButton;
+    private JButton deleteUserButton;
+    private JButton searchUserButton;
+
     public adminpage() {
         setTitle("Admin Dashboard");
         setSize(600, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        addBookButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        deleteUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        searchUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
+
+
+
 }
